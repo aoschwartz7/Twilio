6036d263c11dcfc08f4d35be8dfd9e0fdadbebd6
Alec Schwartz <aoschwartz7@gmail.com>
2020-04-07 11:10:57 -0400

    creating functions for random riddles / answers

--- riddleCode.py
+++ riddleCode.py
@@ -1,8 +1,39 @@
 import random
 import json
-from riddles import riddles
+from riddleBank import riddleBank
 
-riddlesLoaded = json.loads(riddles)
 
-def randomRiddle():
-    return random.sample(riddlesLoaded, 1)[0]
+# function name: randomRiddle()
+# parameters: riddles
+# application: load riddles and return a random riddle
+# output: random riddle
+# called by: send_sms.py
+def randomRiddle(riddles):
+    riddlesLoaded = json.loads(riddles)
+    return random.sample(list(riddlesLoaded), 1)
+
+
+# function name: riddleAnswer()
+# parameters: riddleBank, riddle
+# application: Return riddle's correctAnswer from riddleBank.
+# output: correctAnswer
+# function called by:
+def riddleAnswer(riddleBank, riddle):
+    correctAnswer = riddleBank[riddle]
+    return correctAnswer
+
+
+# function name: cleanString()
+# parameters: stringToClean
+# application: Make user's response more flexible by lower-casing answer.
+# output: stringToClean
+# function called by:
+def cleanString(stringToClean):
+    stringToClean = stringToClean.lower()
+    return stringToClean
+
+if __name__ == '__main__':
+    riddle = randomRiddle(riddleBank)
+    print(riddle)
+    print(riddleAnswer(riddleBank, riddle))
+    print(riddleAnswer)
